0ff6ef47cc1cf37dc38459c37998189e2c518330
Shbinging <bingshui@smail.nju.edu.cn>
2025-04-03 07:26:45 +0000

    [fuzzing] support isis

--- build_fuzzing.sh
+++ build_fuzzing.sh
@@ -1,7 +1,7 @@
 export ASAN_OPTIONS=detect_leaks=0 
-export CFLAGS="${CFLAGS} -DFUZZING_OVERRIDE_LLVMFuzzerTestOneInput -fprofile-instr-generate -fcoverage-mapping "
+export CFLAGS="${CFLAGS} -g -DFUZZING_OVERRIDE_LLVMFuzzerTestOneInput -fprofile-instr-generate -fcoverage-mapping "
 export CC=clang 
 ./bootstrap.sh
-./configure --enable-libfuzzer --enable-address-sanitizer --enable-static --enable-static-bin 
+./configure --enable-libfuzzer --enable-address-sanitizer --enable-static --enable-static-bin --disable-fabricd --disable-pbrd
 make -j$(nproc) 
 sudo make install
